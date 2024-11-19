4636acddf35de541c0accbda1662c119745264ea
Mauricio <mcardenas@ucol.mx>
2024-11-19 12:21:23 -0600

    feat(xf)

--- src/State/PasswordHasher.php
+++ src/State/PasswordHasher.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace App\State;
 
@@ -6,28 +6,34 @@ use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
 use App\Entity\User;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-
+/**
+ * @implements ProcessorInterface<mixed,mixed>
+ */
 final readonly class PasswordHasher implements ProcessorInterface
 {
-	public function __construct(
-		private ProcessorInterface $processor,
-		private UserPasswordHasherInterface $passwordHasher
-	)
-	{}
+    /**
+     * @param ProcessorInterface<mixed,mixed> $processor
+     */
+    public function __construct(
+        private ProcessorInterface $processor,
+        private UserPasswordHasherInterface $passwordHasher
+    ) {
+    }
 
-	/**
+    /**
      * @param User $data
      */
-    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): User
-    {
+    public function process(
+        mixed $data,
+        Operation $operation,
+        array $uriVariables = [],
+        array $context = []
+    ): User {
         if (!$data->getPlainPassword()) {
             return $this->processor->process($data, $operation, $uriVariables, $context);
         }
 
-        $hashedPassword = $this->passwordHasher->hashPassword(
-            $data,
-            $data->getPlainPassword()
-        );
+        $hashedPassword = $this->passwordHasher->hashPassword($data, $data->getPlainPassword());
         $data->setPassword($hashedPassword);
         $data->eraseCredentials();
 
